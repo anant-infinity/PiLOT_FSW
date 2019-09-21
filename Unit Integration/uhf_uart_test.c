8b7ff168032aa84ff37b79652a3865c43566c707
Anant <278anant@gmai.com>
2019-09-21 17:19:40 +0530

    Tested CoreUART and the MSS_UART
    Tested the CoreUART , added the different base addresses. Also tested the MSS UART .

--- Unit Integration/uhf_uart_test.c
+++ Unit Integration/uhf_uart_test.c
@@ -1,58 +1,58 @@
-#define MAX_RX_DATA_SIZE 512
-//change this according to the PiLOT FGPA prject 
-//#define COREUART3_BASE_ADDR 	0x5000D000u
-//#define COREUART3_BAUD_VALUE 	BAUD_VALUE_115200
-//UART_instance_t uhf_test_uart;
+#include "mss_i2c.h" //so that the main code will not be too long just for declaration
+#include "core_uart_apb.h"
+#include "mss_uart.h"
+
+#define COREUART0_BASE_ADDR 	0x50000000u
+#define COREUART1_BASE_ADDR 	0x50001000u
+#define COREUART2_BASE_ADDR 	0x50002000u
+
+//This value needs to be calculated depending on the speed of operation
+//Can refer IS4
+#define BAUD_VALUE_57600    25
+#define MAX_RX_DATA_SIZE 256
+
+#define TX_LENGTH 1u
+#define RX_LENGTH 1u
+#define MSS_I2C_DUMMY_ADDR 0x10u
+
+UART_instance_t rs422_test_uart;
+
 uint8_t rx_data[MAX_RX_DATA_SIZE];
 size_t rx_size = 0;
+
+uint8_t rx_data[MAX_RX_DATA_SIZE];
 
 int main() {
 	//Turn off the watchdog
 	SYSREG->WDOG_CR = 0; //watchdog on/off is purposely separated from peripheral init
 
-//*********************************************** PiLOT UHF Test S1*************************************\\
-	//Change this according to the PiLOT FPGA project 
+//*********************************************** UART Testing S1**********************
+	//MSS_UART0 working , MSS_UART_1 - Working
+	//CoreUART2 with Isolator U21 - working
+	//Initializing the AtmoLite Command UART
+		UART_init(&rs422_test_uart, COREUART2_BASE_ADDR, BAUD_VALUE_57600, (DATA_8_BITS | NO_PARITY));
+		MSS_UART_init(&g_mss_uart1, MSS_UART_57600_BAUD, MSS_UART_DATA_8_BITS | MSS_UART_NO_PARITY | MSS_UART_ONE_STOP_BIT);
+		//AtmoLite Read Protocol Version Command
+		uint8_t atmolite_read_protocol_ver[] = {0x55};
 
-	//UHF uses ArmCoreUart3 - This corresponds to TX_2 and RX_2
-	//TX_2 - K17 - BB - P13 , Pin 22
-	//RX_2 - B1 - BB - P14 , Pin 22
-	//GND - BB - P7 , Pin 2
 
+		while(1){
+		//Sending a particular UART command
+		//MSS_UART_polled_tx(&g_mss_uart1,(const uint8_t *)&atmolite_read_protocol_ver,sizeof(atmolite_read_protocol_ver));
 
-	//Initializing the UHF UART
-	//Change the baud rate according to the Baud Rate of the PiLOT UHF module
-	UART_init(&uhf_test_uart, COREUART3_BASE_ADDR,COREUART3_BAUD_VALUE, (DATA_8_BITS | NO_PARITY)); 
-	//MSS_UART - if needed
-	//MSS_UART_init( &g_mss_uart0,MSS_UART_57600_BAUD, MSS_UART_DATA_8_BITS | MSS_UART_NO_PARITY |MSS_UART_ONE_STOP_BIT );
-
-	//All read commands without CRC
-	//Command to read Status Control Word
-	uint8_t read_test_command[] = {'E','S','+','R','2','2','0','0','\r'};
-
-	//Id we want to implement CCSDS protocol in the PiLOT mission 
-	//Some information about the CCSDS Header
-	//Packet structure is: (0000 1000) (APP_ID) (GGSS SSSS)(SSSS SSSS) (DDDD DDDD)(DDDD DDDD)(TIME Stamp 5 bytes)(Data)
-	//(0000 1000) the 5th bit is the secondary header flag, 1 if TRUE
-	//GG - grouping Flag - 10 or 11
-	//SS SSSS SSSS SSSS - 00 0000 0000 0001
-	//DDDD DDDD DDDD DDDD - (8+5-1 = 12) - 0000 0000 0000 1100
-	//Secondary Header - 5 Byte dummy time stamp - 0x00 , 0x00 , 0x00 ,0xFF, 0x00
-	//Main Data: 1 2 3
-	// 0x08, 0x10 , 0x80 , 0x01 , 0x00, 0x0C , SEC HED , 0x01 , 0x02, 0x03
-	//uint8_t write_beaconMessage[] = {'E','S','+','W','2','2','F','B','1','5',/**/,'\r'};
-	//Add Fletcher Code Here - 2Bytes - Dummy Fletcher - 0x0F 0x0F
-
-	while(1){
-		//Send the command to the UHF radio
-		UART_send(&uhf_test_uart,(const uint8_t *)&read_test_command,sizeof(read_test_command));
-		
-		//Initialize the RX_BUFFER to have all zeros
-		int k = 0;
-		while(k<MAX_RX_DATA_SIZE){
-			rx_data[k]=0;
-			k++;
+		UART_send(&rs422_test_uart,(const uint8_t *)&atmolite_read_protocol_ver,sizeof(atmolite_read_protocol_ver));
 		}
-
-		//Reading data from the Receive Buffer
-		rx_size = UART_get_rx( &g_mss_uart0, rx_data, sizeof(rx_data));
+//		uint8_t k = 0;
+//
+//		//Initialize RX Buffer to zero
+//		while(k<MAX_RX_DATA_SIZE){
+//			rx_data[k]=0;
+//			k++;
+//		}
+//		//Get the Response from the ADCS
+//		//Reading data from the Receive Buffer
+//		rx_size = UART_get_rx(&rs422_test_uart, rx_data, sizeof(rx_data));
+//
+//		}
+	//**************************************************************************************
 	}
